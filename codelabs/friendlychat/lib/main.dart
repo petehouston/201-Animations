097780c503289fcb159344cf1d3add4f56b12fb7
nitya <n.narasimhan@gmail.com>
2018-08-26 14:21:43 -0400

    Add & Refactor: Update with ListView for message display

--- codelabs/friendlychat/lib/main.dart
+++ codelabs/friendlychat/lib/main.dart
@@ -1,5 +1,38 @@
 import 'package:flutter/material.dart';
 
+const String _name = "Andie Droid";
+
+class FriendlyChatMessage extends StatelessWidget {
+  final String text;
+  FriendlyChatMessage({this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0])),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class FriendlyChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -16,11 +49,16 @@ class FriendlyChatScreen extends StatefulWidget {
 }
 
 class FriendlyChatScreenState extends State<FriendlyChatScreen> {
+  final List<FriendlyChatMessage> _messages = <FriendlyChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
 
   void _handleSubmitted(String text) {
     print("Submitted Text: $text");
     _textController.clear();
+    FriendlyChatMessage message = FriendlyChatMessage(text: text);
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 
   Widget _buildTextComposer() {
@@ -57,7 +95,25 @@ class FriendlyChatScreenState extends State<FriendlyChatScreen> {
       appBar: new AppBar(
         title: new Text("Hello! "),
       ),
-      body: _buildTextComposer(),
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+            child: ListView.builder(
+              padding: new EdgeInsets.all(8.0), //new
+              reverse: true, //new
+              itemBuilder: (_, int index) => _messages[index], //new
+              itemCount: _messages.length,
+            ),
+          ),
+          new Divider(height: 1.0),
+          new Container(
+            decoration: new BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
     );
   }
 }
