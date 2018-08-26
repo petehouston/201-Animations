8eaf64a43c6237b1811b770788ec3920fbf5a64f
nitya <n.narasimhan@gmail.com>
2018-08-26 09:28:41 -0400

    Add: Interactive Elements to Scaffold body (in Stateful widget)
    First a Text composer widget that provides a TextField (for input) and TextController (to manage interactions)

--- codelabs/friendlychat/lib/main.dart
+++ codelabs/friendlychat/lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 
-class FriendlyChatApp extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() => 
-    new FriendlyChatAppState();
-}
-
-class FriendlyChatAppState extends State<FriendlyChatApp>{
+class FriendlyChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -16,13 +10,37 @@ class FriendlyChatAppState extends State<FriendlyChatApp>{
   }
 }
 
-class FriendlyChatScreen extends StatelessWidget{
+class FriendlyChatScreen extends StatefulWidget {
+  @override
+  State createState() => new FriendlyChatScreenState();
+}
+
+class FriendlyChatScreenState extends State<FriendlyChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
+  void _handleSubmitted(String text){
+    print("Submitted Text: $text");
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: new InputDecoration.collapsed(hintText: "Send a message"),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Hello! "),
       ),
+      body: _buildTextComposer(),
     );
   }
 }
