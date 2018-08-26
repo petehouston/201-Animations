8951aba6cc42cc6245b4158518cfc2560455755a
nitya <n.narasimhan@gmail.com>
2018-08-26 09:43:07 -0400

    Add & Refactor: Adding Send button + handling interaction events

--- codelabs/friendlychat/lib/main.dart
+++ codelabs/friendlychat/lib/main.dart
@@ -18,18 +18,35 @@ class FriendlyChatScreen extends StatefulWidget {
 class FriendlyChatScreenState extends State<FriendlyChatScreen> {
   final TextEditingController _textController = new TextEditingController();
 
-  void _handleSubmitted(String text){
+  void _handleSubmitted(String text) {
     print("Submitted Text: $text");
     _textController.clear();
   }
 
   Widget _buildTextComposer() {
-    return new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: new InputDecoration.collapsed(hintText: "Send a message"),
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
