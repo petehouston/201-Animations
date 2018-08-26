3ce262419589eb3640e7f7215351e8957e10483a
nitya <n.narasimhan@gmail.com>
2018-08-26 15:15:33 -0400

    Final: Use defaultTargetPlatform property to tailor styles to platform-specific needs

--- codelabs/friendlychat/lib/main.dart
+++ codelabs/friendlychat/lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 
 const String _name = "Andie Droid";
+
+/* Android vs. iOS Themes */
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 
 class FriendlyChatMessage extends StatelessWidget {
   final String text;
@@ -25,15 +39,17 @@ class FriendlyChatMessage extends StatelessWidget {
               margin: const EdgeInsets.only(right: 16.0),
               child: new CircleAvatar(child: new Text(_name[0])),
             ),
-            new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                new Text(_name, style: Theme.of(context).textTheme.subhead),
-                new Container(
-                  margin: const EdgeInsets.only(top: 5.0),
-                  child: new Text(text),
-                ),
-              ],
+            Expanded(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(_name, style: Theme.of(context).textTheme.subhead),
+                  new Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: new Text(text),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -47,6 +63,9 @@ class FriendlyChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Friendly Chat",
+      theme: defaultTargetPlatform == TargetPlatform.iOS //new
+          ? kIOSTheme //new
+          : kDefaultTheme,
       home: new FriendlyChatScreen(),
     );
   }
@@ -61,10 +80,14 @@ class FriendlyChatScreenState extends State<FriendlyChatScreen>
     with TickerProviderStateMixin {
   final List<FriendlyChatMessage> _messages = <FriendlyChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
+  bool _isComposing = false; //new
 
   void _handleSubmitted(String text) {
     print("Submitted Text: $text");
     _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
     FriendlyChatMessage message = FriendlyChatMessage(
       text: text,
       animationController: new AnimationController(
@@ -89,16 +112,30 @@ class FriendlyChatScreenState extends State<FriendlyChatScreen>
               child: new TextField(
                 controller: _textController,
                 onSubmitted: _handleSubmitted,
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
                 decoration:
                     new InputDecoration.collapsed(hintText: "Send a message"),
               ),
             ),
             new Container(
               margin: const EdgeInsets.symmetric(horizontal: 4.0),
-              child: new IconButton(
-                icon: new Icon(Icons.send),
-                onPressed: () => _handleSubmitted(_textController.text),
-              ),
+              child: Theme.of(context).platform == TargetPlatform.iOS
+                  ? new CupertinoButton(
+                      child: new Text("Send"),
+                      onPressed: _isComposing
+                          ? () => _handleSubmitted(_textController.text)
+                          : null,
+                    )
+                  : new IconButton(
+                      icon: new Icon(Icons.send),
+                      onPressed: _isComposing
+                          ? () => _handleSubmitted(_textController.text)
+                          : null,
+                    ),
             ),
           ],
         ),
@@ -107,8 +144,8 @@ class FriendlyChatScreenState extends State<FriendlyChatScreen>
   }
 
   @override
-  void dispose(){
-    for (FriendlyChatMessage message in _messages){
+  void dispose() {
+    for (FriendlyChatMessage message in _messages) {
       message.animationController.dispose();
     }
     super.dispose();
@@ -119,25 +156,35 @@ class FriendlyChatScreenState extends State<FriendlyChatScreen>
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Hello! "),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
       ),
-      body: new Column(
-        children: <Widget>[
-          new Flexible(
-            child: ListView.builder(
-              padding: new EdgeInsets.all(8.0), //new
-              reverse: true, //new
-              itemBuilder: (_, int index) => _messages[index], //new
-              itemCount: _messages.length,
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new Flexible(
+              child: ListView.builder(
+                padding: new EdgeInsets.all(8.0), //new
+                reverse: true, //new
+                itemBuilder: (_, int index) => _messages[index], //new
+                itemCount: _messages.length,
+              ),
             ),
-          ),
-          new Divider(height: 1.0),
-          new Container(
-            decoration: new BoxDecoration(
-              color: Theme.of(context).cardColor,
+            new Divider(height: 1.0),
+            new Container(
+              decoration: new BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: _buildTextComposer(),
             ),
-            child: _buildTextComposer(),
-          ),
-        ],
+          ],
+        ),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? new BoxDecoration(
+                border: new Border(
+                  top: new BorderSide(color: Colors.grey[200]),
+                ),
+              )
+            : null,
       ),
     );
   }
